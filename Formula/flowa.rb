10de45d748b8f678b84ee108ab5299d5e10e13f6
Sayan Senapati <sayansenapati2544@gmail.com>
2025-12-01 08:16:43 +0530

    Bump flowa to v0.1.1

--- Formula/flowa.rb
+++ Formula/flowa.rb
@@ -1,6 +1,5 @@
 class Flowa < Formula
   desc "Pipeline-first language for modern flows"
-  created_by "senapati484"
   homepage "https://github.com/senapati484/flowa"
   url "https://github.com/senapati484/flowa/archive/refs/tags/v0.1.1.tar.gz"
   sha256 "1d3a6c73fb6bc6147427b8e00e73300b340cf49dcb0c86b12cdebfd9128af421"
