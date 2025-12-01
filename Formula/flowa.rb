f3d0684d8e92ac625bf38b43bc3424c1dc5bf41a
Sayan Senapati <sayansenapati2544@gmail.com>
2025-12-01 08:06:38 +0530

    Bump flowa to v0.1.1

--- Formula/flowa.rb
+++ Formula/flowa.rb
@@ -1,14 +1,15 @@
 class Flowa < Formula
-  desc "Your fast, Python + Go inspired language"
+  desc "Pipeline-first language for modern flows"
   homepage "https://github.com/senapati484/flowa"
-  url "https://github.com/senapati484/flowa/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "118e3ddf4bd18fc58cf76976bc5075013705b6a7bbc13a6c44f2e1c45b8c8cb5"
+  url "https://github.com/senapati484/flowa/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "28bc2adf250d575dc56fa8fa397b002aa21536b6444ab0052d4c7c4a560416f3"
+  version "0.1.1"
   license "MIT"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args, "./cmd/flowa"
+    system "go", "build", *std_go_args(output: bin/"flowa"), "./cmd/flowa"
   end
 
   test do
