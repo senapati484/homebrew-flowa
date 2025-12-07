7181ec2238a17f891b2e19f17d6a0cbdc975596c
Sayan Senapati <sayansenapati2544@gmail.com>
2025-12-07 18:31:20 +0530

    Fix sha256 for flowa 0.1.3

--- Formula/flowa.rb
+++ Formula/flowa.rb
@@ -1,10 +1,10 @@
 class Flowa < Formula
   desc "Flowa JIT Compiler - A V8-style JIT for backend systems"
   homepage "https://github.com/senapati484/flowa"
-  url "https://github.com/senapati484/flowa/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "ced9ffe5b674d55d15fd7c13d9d02064ef1ffb967da74426e2041ca516a4ad74"
+  url "https://github.com/senapati484/flowa/archive/refs/tags/v0.1.4.tar.gz"
+  sha256 "1ebefa5e13082ad93d5acfdd8ec6d20638bf9f9d363ddd7d13039b73aabb5e89"
   license "MIT"
-  version "0.1.3"
+  version "0.1.4"
 
   depends_on "cmake" => :build
   depends_on "llvm" => :build
