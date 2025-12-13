ab8d06b64bd5f48744955f6d8b63884c2be162ed
Sayan Senapati <sayansenapati2544@gmail.com>
2025-12-13 09:26:32 +0530

    adding sha256 into the repo for verification

--- Formula/flowa.rb
+++ Formula/flowa.rb
@@ -2,7 +2,7 @@ class Flowa < Formula
   desc "Flowa: A high-performance scripting language"
   homepage "https://flowa-docs.vercel.app"
   url "https://raw.githubusercontent.com/senapati484/homebrew-flowa/main/flowa.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "38e197a8f58db9502168cea6bb298c135635b458ac457938863745292b9e872a"
   version "0.1.5"
   license "MIT"
 
