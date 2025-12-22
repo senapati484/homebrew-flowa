be038cba5f63d07809280beb95704deb07c13129
Sayan Senapati <sayansenapati2544@gmail.com>
2025-12-22 22:37:16 +0530

    Force Homebrew refresh: rename archive to flowa-0.1.7.tar.gz

--- Formula/flowa.rb
+++ Formula/flowa.rb
@@ -1,7 +1,7 @@
 class Flowa < Formula
   desc "Flowa: A high-performance scripting language with JIT compilation"
   homepage "https://flowa-docs.vercel.app"
-  url "https://raw.githubusercontent.com/senapati484/homebrew-flowa/main/flowa.tar.gz"
+  url "https://raw.githubusercontent.com/senapati484/homebrew-flowa/main/flowa-0.1.7.tar.gz"
   version "0.1.7"
   sha256 "ce3531e385953148fc0e004f0c78d42ff3ebc8897f2e6dee387481aa0a03685a"
   license "MIT"
